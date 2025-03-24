04d3f15ae0a574702e10eb1a2882e8487d825663
UrayMR <umrajsyafiq@gmail.com>
2025-03-24 15:50:46 +0700

    ref: refactoring migrate tables

--- migrate.sql
+++ migrate.sql
@@ -1,34 +1,34 @@
 CREATE TABLE Fakultas (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Prodi (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     idFakultas INT,
-    FOREIGN KEY (idFakultas) REFERENCES Fakultas(id)
+    FOREIGN KEY (idFakultas) REFERENCES Fakultas(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Mahasiswa (
     npm VARCHAR(20) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     idProdi INT,
-    FOREIGN KEY (idProdi) REFERENCES Prodi(id)
+    FOREIGN KEY (idProdi) REFERENCES Prodi(id) ON DELETE CASCADE
 );
 
 CREATE TABLE MataKuliah (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     idProdi INT,
-    FOREIGN KEY (idProdi) REFERENCES Prodi(id)
+    FOREIGN KEY (idProdi) REFERENCES Prodi(id) ON DELETE CASCADE
 );
 
 CREATE TABLE RuanganKelas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     idFakultas INT,
-    FOREIGN KEY (idFakultas) REFERENCES Fakultas(id)
+    FOREIGN KEY (idFakultas) REFERENCES Fakultas(id) ON DELETE CASCADE
 );
 
 CREATE TABLE KRS (
@@ -36,7 +36,7 @@ CREATE TABLE KRS (
     npm VARCHAR(20),
     idMataKuliah INT,
     FOREIGN KEY (npm) REFERENCES Mahasiswa(npm) ON DELETE CASCADE,
-    FOREIGN KEY (idMataKuliah) REFERENCES Mata_Kuliah(id) ON DELETE CASCADE
+    FOREIGN KEY (idMataKuliah) REFERENCES MataKuliah(id) ON DELETE CASCADE
 );
 
 SELECT m.npm, m.name AS Mahasiswa, p.name AS Prodi, f.name AS Fakultas, COUNT(mk.id) AS MataKuliahCount
